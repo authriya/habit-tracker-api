a0938ce1b7d9f17178f3bd6a6c81c0b0b9b80a05
Supriya Ganesh <supriya@thinkful.com>
2020-04-29 22:30:49 -0400

    debugging

--- src/config.js
+++ src/config.js
@@ -3,5 +3,5 @@ module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://supriyaganesh@localhost/habit_app',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://supriyaganesh@localhost/habit_app_test',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://habit-tracker-app.now.sh'
 }
